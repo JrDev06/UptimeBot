e799eb5984e3b28dde1e7e97c2a369f93402bf18
JrDev06 <jrbusaco72@gmail.com>
2024-06-10 20:59:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const botConfig = {
   username: 'RiseSMPUptimeBOt',
   port: 46779,
   plugins: [AutoAuth],
-  AutoAuth: 'RiseSMPBOT06'
+  AutoAuth: 'RiseSMPBOT062024'
 };
 
 // Create bot function
