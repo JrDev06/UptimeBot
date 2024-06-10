f687587ec33e8d0c6cf30134f52dbd200a0a46fe
JrDev06 <jrbusaco72@gmail.com>
2024-06-10 12:07:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (_, res) => res.sendFile(__dirname + "/index.html"));
 app.listen(process.env.PORT);
 
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.repl.co/`);
+  http.get(`https://uptimebot-t4z5.onrender.com`);
 }, 224000);
 
 
